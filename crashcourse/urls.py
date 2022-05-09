55b27f2ae7a65396e0e5f3037ad9c2d957b5a3eb
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-05-09 12:09:11 +0430

    Display todo lists

--- crashcourse/urls.py
+++ crashcourse/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('tasks', include('todo.urls')),
+    path('', include('todo.urls')),
     path('admin/', admin.site.urls),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Todo
+
 
 def todo_home_view(request):
-
-    return render(request, "todo/home.html")
+    tasks = Todo.objects.all()
+    context = {
+        'tasks': None,
+    }
+    if tasks:
+        context['tasks'] = tasks
+    return render(request, "todo/home.html", context=context)
