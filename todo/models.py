35dbd657c9dc6a58bd4368424af1e06ba172a168
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-05-11 12:32:16 +0430

    Change model to host the created date

--- todo/models.py
+++ todo/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Todo(models.Model):
     task_name = models.CharField(max_length=100)
     due_date = models.DateField()
+    created_at = models.DateField(
+        auto_now_add=True, auto_created=True)
 
     def __str__(self):
         return self.task_name
